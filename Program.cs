5a931c4e56b69eb161107be4a3f8240e339306a3
Bruno César Lima Araújo <1561719@sga.pucminas.br>
2025-02-10 22:23:50 -0300

    Segundo commit

--- Program.cs
+++ Program.cs
@@ -2,13 +2,15 @@
 class Program {
     public static void Main (string[] args) {
         int supermercados = int.Parse(Console.ReadLine());
-        string linha = Console.ReadLine();
-        string[] preco = linha.Split(' ');
-        double p = double.Parse(preco[0]);
-        int g = int.Parse(preco[1]);
         double menorPreco = 100;
-        if (p < menorPreco)
-        menorPreco = p;
+        for (int i = 0; i < supermercados; i++) {
+            string linha = Console.ReadLine();
+            string[] preco = linha.Split(' ');
+            double p = double.Parse(preco[0]);
+            int g = int.Parse(preco[1]);
+            if (p < menorPreco)
+            menorPreco = p;
+        }
         Console.WriteLine($"{menorPreco:F2}");
     }
 }
