23ab8057b2364b75cdd9296965e79e77e2e45cf0
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2025-02-26 14:34:33 -0300

    Tomar no cu viu Linus Torvauds, faça uma porra de um gerenciador de versões que preste da próxima vez

--- obj/Debug/net9.0/tp0Aed.AssemblyInfo.cs
+++ obj/Debug/net9.0/tp0Aed.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp0Aed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a931c4e56b69eb161107be4a3f8240e339306a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4764b69e0bd70aec3c3e125dd6d4c091bd3a6882")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp0Aed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp0Aed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
